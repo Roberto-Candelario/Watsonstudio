a8e00d8ec7336259931cef98357924c42582c635
Roberto-Candelario <104696539+Roberto-Candelario@users.noreply.github.com>
2022-07-21 14:02:09 -0400

    Add files via upload

---
+++ Python env.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# **Roberto Candelario**  
+# Data Scientist 
+
+# *I am interested in dat sciience because data is a huge part of our lives. Data can be used to help make better descisions. Wheather that may be for a company, a department or an individual.*
+
+# ### In The code below I am inputting two numbers into a set of variables. X and Y.  The variables will perform a simple calculation
+
+# In[7]:
+
+
+x = 5
+y = 10
+x + 10
+
+
+# Horizontal rule
+# ***
+#   
+#     
+# ```python
+# s = "Python syntax highlighting"
+# print s
+# ```
+#   
+#   
+# > Blocked quotes are greate   
+# > to have multiple lines   
+# > and the reader visually see  
+# > that it all goes togeether   
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
